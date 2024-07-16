88c03b7649e5ca5e08473c03ee98d187ebd41290
Cinzya <cynti-ebert@web.de>
2024-07-16 05:49:34 +0200

    feat: highlight tooltip when text was copied

--- copy.js
+++ copy.js
@@ -1,18 +1,20 @@
-  function copy(text, id) {
-    var input = document.createElement('input');
-    input.setAttribute('type', 'text');
-    input.setAttribute('value', text);
-    /* Get the text field */
-    var copyText = input;
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value);
-    
-    var tooltip = document.getElementById(id);
-    tooltip.innerHTML = "Copied";
-  }
-  
-  function out(id) {
-    var tooltip = document.getElementById(id);
-    tooltip.innerHTML = "Copy to clipboard";
-  }
+function copy(text, id) {
+  var input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("value", text);
+  /* Get the text field */
+  var copyText = input;
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+
+  var tooltip = document.getElementById(id);
+  tooltip.classList.add("clicked");
+  tooltip.innerHTML = "Copied!";
+}
+
+function out(id) {
+  var tooltip = document.getElementById(id);
+  tooltip.classList.remove("clicked");
+  tooltip.innerHTML = "Copy to clipboard";
+}
